ec4f8e63085a5c753172a549599b954ef00b7dc3
Svetlana Mikhaylova <idillia8709@yandex.ru>
2021-01-25 15:34:04 +0300

    добавлен класс Popup

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -7,28 +7,28 @@ export default class Popup {
 
   _closeByEsc(event) {
     if (event.key === 'Escape') {
-      this._close();
+      this.close();
     }
   }
 
-  _close() {
-    this._element.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._closeByEsc);
-  }
-
+  
   _closeByOverlayClick(event) {
     if (event.target === this._element) {
-      this._close();
+      this.close();
     }
   }
-
+  
   _initListeners() {
-    this._closeBtn.addEventListener('click', () => this._close());
+    this._closeBtn.addEventListener('click', () => this.close());
     this._element.addEventListener('click', (event) => this._closeByOverlayClick(event));
   }
-
+  
   open() {
     this._element.classList.add('popup_opened');
     document.addEventListener('keydown', (event) => this._closeByEsc(event));
+  }
+  close() {
+    this._element.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._closeByEsc);
   }
 }
